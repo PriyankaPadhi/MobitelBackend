b97b40b7a2e8d80753959ed87a5344f3a3cda539
PRIYANKA PADHI <ppadhi40@gmail.com>
2017-07-18 21:49:02 +0400

    9th commit

---
+++ MobitelBackend/src/main/java/com/mobitel/App.java
@@ -0,0 +1,13 @@
+package com.mobitel;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
